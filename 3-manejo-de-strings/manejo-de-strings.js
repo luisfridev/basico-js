dd0cf29f523c2ebec927b979c352aaf8b5481f14
Luis <luisangeldev@gmail.com>
2026-09-07 12:43:03 -0500

    Manejo de strings

---
+++ 3-manejo-de-strings/manejo-de-strings.js
@@ -0,0 +1,17 @@
+// Tipo de dato string
+
+let string1 = 'Hola mundo'
+let string2 = "Javacript es genial"
+let string3 = `${string1} feliz :)`
+let string4 = string1 + ' ' + string2
+
+console.log(string1)
+console.log(string2)
+console.log(string3)
+console.log(string4)
+
+let frase = 'JavaScript es un Extremadamente genial'
+console.log(frase.length) // contar la cantidad de caracteres
+console.log(frase.toUpperCase()) // convertir a mayusculas
+console.log(frase.toLowerCase()) // convertir a minusculas
+console.log(frase.substring(0, 10)) // extraer una parte de la cadena
